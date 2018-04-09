70d78cbbf69c4c14fe92483bf28142487b3b7c80
brant9527 <398707650@qq.com>
2018-04-10 00:50:56 +0800

    增加适配

---
+++ src/views/login/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="">
+      <div class="text">登陆</div>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang='scss'>
+  .text{
+    font-size: px(100);
+  }
+</style>
